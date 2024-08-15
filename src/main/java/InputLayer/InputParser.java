dba6dc2a5a8bdfffe586f4627e1e4d76c93aeea4
Joeltt <joelttm15@gmail.com>
2024-08-15 16:49:02 +0100

    add parseInputToPosition method

--- src/main/java/InputLayer/InputParser.java
+++ src/main/java/InputLayer/InputParser.java
@@ -8,7 +8,25 @@ public class InputParser {
 
     public String input;
 
-    public ArrayList<Instruction> parseStringToInstruction(String input) {
+    public Instruction checkInstruction (String command) {
+        for (Instruction validCommand : Instruction.values()) {
+            if (validCommand.name().equalsIgnoreCase(command)) {
+                return Instruction.valueOf(command.toUpperCase());
+            }
+        }
+        return null;
+    }
+
+    public CompassDirection checkCompassDirection(String direction) {
+            for (CompassDirection validCommand : CompassDirection.values()) {
+                if (validCommand.name().equalsIgnoreCase(direction)) {
+                    return CompassDirection.valueOf(direction.toUpperCase());
+                }
+        }
+        throw new IllegalArgumentException("No direction details found!");
+    }
+
+    public ArrayList<Instruction> parseInputToInstruction(String input) {
         if(input.isEmpty()) {
             System.out.println("No input found!");
             throw new RuntimeException("No input found!");
@@ -22,33 +40,20 @@ public class InputParser {
                 throw new IllegalArgumentException("Invalid Input found!");
             }
         }
-    return listOfCommands;
+        return listOfCommands;
     }
 
-    public Instruction checkInstruction (String command) {
+    public Position parseInputToPosition(String input) {
 
-        for (Instruction validCommand : Instruction.values()) {
-            if (validCommand.name().equals(command)) {
-                return Instruction.valueOf(command);
-            }
+        input = input.replace(" ", "");
+        if (input.isEmpty() || input.length() > 3) {
+            throw new IllegalArgumentException("input is invalid!");
         }
-        return null;
+        String[] inputArray = input.split("");
+        CompassDirection facingDirection = checkCompassDirection(String.valueOf(input.charAt(inputArray.length - 1)));
+        return new Position(Integer.parseInt(inputArray[0]), Integer.parseInt(inputArray[1]), facingDirection);
     }
 
-    public CompassDirection checkCompassDirection(String position) {
-        if (position.isEmpty()) {
-            System.out.println("No input found!");
-            throw new RuntimeException("No input found!");
-        }
-        String[] positionArray = position.split("");
-        for (String direction : positionArray) {
-            for (CompassDirection validCommand : CompassDirection.values()) {
-                if (validCommand.name().equals(direction)) {
-                    System.out.println("Rover is facing: " + direction);
-                    return CompassDirection.valueOf(direction);
-                }
-            }
-        }
-        throw new IllegalArgumentException("No direction details found!");
-    }
+
+
 }
