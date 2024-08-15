61a589cd5bdb5f61ca3c31a8ef8a69d181d72f36
Joeltt <joelttm15@gmail.com>
2024-08-15 16:50:26 +0100

    feat:add more test for parseInputToPosition method

--- src/test/java/InputLayer/InputParserTest.java
+++ src/test/java/InputLayer/InputParserTest.java
@@ -3,12 +3,11 @@ package InputLayer;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.function.Executable;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 
 import static InputLayer.Instruction.*;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class InputParserTest {
@@ -25,7 +24,25 @@ class InputParserTest {
     public void testCheckForInstruction_validInput() {
         String input = "LRMMRM";
 
-        ArrayList<Instruction> result = inputParser.parseStringToInstruction(input);
+        ArrayList<Instruction> result = inputParser.parseInputToInstruction(input);
+        ArrayList<Instruction> expectedResult = new ArrayList<>(){{
+            add(L);
+            add(R);
+            add(M);
+            add(M);
+            add(R);
+            add(M);
+        }};
+
+        assertEquals(expectedResult, result);
+    }
+
+    @Test
+    @DisplayName("return list of instruction given a valid string of instruction but in lowerCase")
+    public void testCheckForInstruction_validLowerCaseInput() {
+        String input = "lrmmrm";
+
+        ArrayList<Instruction> result = inputParser.parseInputToInstruction(input);
         ArrayList<Instruction> expectedResult = new ArrayList<>(){{
             add(L);
             add(R);
@@ -43,7 +60,7 @@ class InputParserTest {
     public void testCheckForInstruction_invalidInput() {
         String input = "LRDMMDSRM";
 
-        assertThrows(IllegalArgumentException.class, () -> inputParser.parseStringToInstruction(input));
+        assertThrows(IllegalArgumentException.class, () -> inputParser.parseInputToInstruction(input));
     }
 
     @Test
@@ -51,13 +68,24 @@ class InputParserTest {
     public void testCheckForInstruction_emptyInput() {
         String input = "";
 
-        assertThrows(RuntimeException.class, () -> inputParser.parseStringToInstruction(input));
+        assertThrows(RuntimeException.class, () -> inputParser.parseInputToInstruction(input));
     }
 
     @Test
     @DisplayName("return a CompassDirection given a valid string of position")
     public void testCheckForCompassDirection_validInput() {
-        String input = "1 3 N";
+        String input = "E";
+
+        CompassDirection result = inputParser.checkCompassDirection(input);
+        CompassDirection expectedResult = CompassDirection.E;
+
+        assertEquals(expectedResult, result);
+    }
+
+    @Test
+    @DisplayName("return a CompassDirection given a valid string of position but in lowercase")
+    public void testCheckForCompassDirection_validLowerCaseInput() {
+        String input = "n";
 
         CompassDirection result = inputParser.checkCompassDirection(input);
         CompassDirection expectedResult = CompassDirection.N;
@@ -71,6 +99,28 @@ class InputParserTest {
         String input = "1 3 2";
 
         assertThrows(IllegalArgumentException.class, () -> inputParser.checkCompassDirection(input));
+    }
+
+    @Test
+    @DisplayName("return a position given a valid string of position but in lowercase")
+    public void testCheckForParseInputToPosition_validInput() {
+        String input = "1 2 N";
+        Position expectedResult = new Position (1, 2, CompassDirection.N);
+        Position result = inputParser.parseInputToPosition(input);
+
+
+        assertThat(result).usingRecursiveComparison().isEqualTo(expectedResult); //using assertj library to check the fields of the two
+    }
+
+    @Test
+    @DisplayName("return a position given a valid string of position but in lowercase")
+    public void testCheckForParseInputToPosition_validLowerCaseInput() {
+        String input = "1 2 n";
+
+        Position result = inputParser.parseInputToPosition(input);
+        Position expectedResult = new Position(1, 2, CompassDirection.N);
+
+        assertThat(result).usingRecursiveComparison().isEqualTo(expectedResult);
     }
 
 
