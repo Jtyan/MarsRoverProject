e7e8b30875e82feabd7684e1050f84b27ec66581
Joeltt <joelttm15@gmail.com>
2024-08-15 16:53:29 +0100

    feat: add rover class

---
+++ src/main/java/LogicLayer/Rover.java
@@ -0,0 +1,19 @@
+package LogicLayer;
+
+import InputLayer.Position;
+
+public class Rover {
+    private Position position;
+
+    public Rover(Position position) {
+        this.position = position;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public void setPosition(Position position) {
+        this.position = position;
+    }
+}
