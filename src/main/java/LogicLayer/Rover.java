dc99162a4ef5c074a48d9c55d11797a7e3e67f4e
Joeltt <joelttm15@gmail.com>
2024-08-16 11:37:12 +0100

    feat: add rotate method for Rover class

--- src/main/java/LogicLayer/Rover.java
+++ src/main/java/LogicLayer/Rover.java
@@ -1,6 +1,11 @@
 package LogicLayer;
 
+import InputLayer.CompassDirection;
+import InputLayer.Instruction;
 import InputLayer.Position;
+import com.sun.source.tree.ReturnTree;
+
+import static InputLayer.CompassDirection.*;
 
 public class Rover {
     private Position position;
@@ -15,5 +20,19 @@ public class Rover {
 
     public void setPosition(Position position) {
         this.position = position;
+    }
+
+    public void rotate(Instruction instruction) {
+        CompassDirection facing = getPosition().getFacing();
+
+        if(instruction.equals(Instruction.L) || instruction.equals(Instruction.R)) {
+            CompassDirection newFacing = switch (facing) {
+                case N -> instruction.equals(Instruction.L) ? W : E;
+                case W -> instruction.equals(Instruction.L) ? S : N;
+                case S -> instruction.equals(Instruction.L) ? E : W;
+                case E -> instruction.equals(Instruction.L) ? N : S;
+            };
+            getPosition().setFacing(newFacing);
+        }
     }
 }
