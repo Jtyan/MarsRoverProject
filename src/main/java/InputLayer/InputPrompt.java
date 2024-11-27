84928c0b8862d03d3daf03480afca34b5a8da1d3
Joeltt <joelttm15@gmail.com>
2024-11-27 15:59:32 +0000

    feat: create an InputPrompt class for user input

--- src/main/java/InputLayer/InputPrompt.java
+++ src/main/java/InputLayer/InputPrompt.java
@@ -1,8 +1,53 @@
 package InputLayer;
 
+import LogicLayer.MissionControl;
+import LogicLayer.Rover;
+
+import java.util.Scanner;
+
 public class InputPrompt {
+    private static Scanner scanner = new Scanner(System.in);
+    private static Plateau plateau  = Plateau.getInstance();
+    private static Rover rover = new Rover();
+    private static MissionControl missionControl = new MissionControl(plateau, rover);
 
-    public static void getInputString(String[] args) {
+    public static void getInputString() {
 
+        createMapInput();
+        deployRoverInput();
+        moveRoverInput();
+
+        System.out.println("Thank you for trying it out. Goodbye!");
+        scanner.close();
+
+    }
+
+    public static void createMapInput() {
+
+        System.out.println("Welcome to Joel's Mars Rover Simulator!");
+        System.out.println("Let us create a map. It must be at least a 2x2 plateau.");
+        System.out.print("First, enter the number of rows for the plateau: ");
+        String rows = scanner.next();
+        System.out.print("Enter the number of columns for the plateau: ");
+        String columns = scanner.next();
+        plateau.setPlateauSize(rows, columns);
+        System.out.println("\nPlateau " + plateau.getRow() + "x" + plateau.getColumn() + " has been created.\n");
+        plateau.generateObstaclesRandomPosition();
+    }
+
+    public static void deployRoverInput() {
+        System.out.println("\nNow, enter the x and y coordinates of the rover along with its initial facing direction(N,S,E,W). e.g: 2,4,N: ");
+        String positionInput = scanner.next();
+        missionControl.deployRover(positionInput);
+    }
+
+    public static void moveRoverInput() {
+        do {
+            System.out.println("Enter the instruction to move rover (L for rotate left, R for rotate right, M to move). e.g: LMLMLMLMM: ");
+            String instructions = scanner.next();
+            System.out.println("Rover initial position: " + rover.getPosition().getX() + "," + rover.getPosition().getY() + "," + rover.getPosition().getFacing() + ".");
+            missionControl.moveRover(instructions);
+            System.out.println("Would you like to continue? (Y/N):");
+        }while (scanner.next().equalsIgnoreCase("Y"));
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,45 +1,15 @@
 package org.example;
 
-import InputLayer.InputParser;
-import InputLayer.Instruction;
-import InputLayer.PlateauSize;
-import InputLayer.Position;
+import InputLayer.*;
 import LogicLayer.MissionControl;
 import LogicLayer.Rover;
 
 import java.util.ArrayList;
+import java.util.Random;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
-        String mapInput = "5, 5";
-        PlateauSize.getInstance().setColumn(5);
-        PlateauSize.getInstance().setRow(5);
-        MissionControl plateau = new MissionControl(PlateauSize.getInstance());
-
-        InputParser inputParser = new InputParser();
-
-        String positionInput = " 3 3, N";
-        Position validStartingPosition = inputParser.parseInputToPosition(positionInput);
-        Rover rover1 = new Rover(validStartingPosition);
-        System.out.println("Initial Position where X: " + rover1.getPosition().getX() + ", Y: " + rover1.getPosition().getY() + ", Faces " + rover1.getPosition().getFacing());
-        plateau.addRover(rover1);
-
-        String movingInput = "LLLLLMMRMMLLLMR"; // Move the rover
-        ArrayList<Instruction> validListOfInstructions = inputParser.parseInputToInstruction(movingInput);
-        rover1.setNewPosition(validListOfInstructions);
-        System.out.println("New Position where X: " + rover1.getPosition().getX() + ", Y: " + rover1.getPosition().getY() + ", Faces " + rover1.getPosition().getFacing());
-
-
-    /*To-do
-      add parseInputToMapGrid
-      what to do with rover moving out of the map
-      what to do with rover colliding with another rover or obstacles
-      make sure if MissionControl class is not too big /complicated
-      linking input layer to logic layer
-      tests for everything!
-    */
-
+        InputPrompt.getInputString();
     }
 }
