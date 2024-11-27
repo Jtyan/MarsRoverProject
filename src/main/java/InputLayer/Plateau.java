0a417b19741232acb28af5ec2f124c8d7512403e
Joeltt <joelttm15@gmail.com>
2024-11-27 10:37:18 +0000

    fix: change generateObstaclesRandomPosition to randomly generate number of obstacles and fix the obstacles position checks

--- src/main/java/InputLayer/Plateau.java
+++ src/main/java/InputLayer/Plateau.java
@@ -41,22 +41,51 @@ public class Plateau {
         this.column = column;
     }
 
-    public void generateObstaclesRandomPosition(Integer numberOfObstacles) {
+    public Set<Position> getObstacles() {
+        return obstacles;
+    }
+
+    public void generateObstaclesRandomPosition() {
+        int totalCells = row * column;
+        int minObstacles = (int) Math.ceil(totalCells / 10.0);
+        int numberOfObstacles = random.nextInt(minObstacles);
+
+        System.out.println("Number of obstacles placed: " + numberOfObstacles);
+
         for (int i = 0; i < numberOfObstacles; i++) {
-            Position newObstaclePosition = new Position(random.nextInt(row), random.nextInt(column));
-            if (obstacles.contains(newObstaclePosition)) {
-                i--;
-            }
+            Position newObstaclePosition;
+            boolean isDuplicate;
+
+            do {
+                newObstaclePosition = new Position(random.nextInt(row), random.nextInt(column));
+                isDuplicate = false;
+
+                for (Position position : obstacles) {
+                    if (position.getX() == newObstaclePosition.getX() &&
+                            position.getY() == newObstaclePosition.getY()) {
+                        isDuplicate = true;
+                        break;
+                    }
+                }
+            } while (isDuplicate);
+
             obstacles.add(newObstaclePosition);
+            System.out.println(newObstaclePosition.getX() + "," + newObstaclePosition.getY());
         }
     }
 
     public boolean hasObstacle(Position position) {
-        return obstacles.contains(position);
+        Position positionToCheck = new Position(position.getX(), position.getY());
+        for (Position obstacle : obstacles) {
+            if (obstacle.getX() == positionToCheck.getX() && obstacle.getY() == positionToCheck.getY()) {
+                return true;
+            }
+        }
+        return false;
     }
 
-    public void setPlateauSize(String mapInput) {
-        ArrayList<Integer> dimension = InputParser.parseInputToPlateau(mapInput);
+    public void setPlateauSize(String rowString, String columnString) {
+        ArrayList<Integer> dimension = InputParser.parseInputToPlateau(rowString, columnString);
         this.setRow(dimension.get(0));
         this.setColumn(dimension.get(1));
     }
